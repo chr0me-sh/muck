77c4ef8a7845366b6c2e219e3a00ea55fbcef6b0
chr0me-sh <lachlan@chr0me.sh>
2019-06-22 14:35:10 +0800

    Added basic formatting/mounting, cleaned up code a bit.

--- muck/muck.py
+++ muck/muck.py
@@ -1,24 +1,10 @@
 import parted
-
-def prep_device(device):
-    with open(device.path, 'wb') as p:
-        p.write(bytearray(1024))
-
-    disk = parted.freshDisk(device, 'msdos')
-    geometry = parted.Geometry(device=device, start=1, length=device.getLength() - 1)
-    filesystem = parted.FileSystem(type='ext4', geometry=geometry)
-    partition = parted.Partition(disk=disk, type=parted.PARTITION_NORMAL,
-                                 fs=filesystem, geometry=geometry)
-
-    disk.addPartition(partition=partition,
-                      constraint=device.optimalAlignedConstraint)
-
-    partition.setFlag(parted.PARTITION_BOOT)
-
-    disk.commit()
-
+import os
+import subprocess
+import glob
 
 def show_devices():
+    """Prints a list of storage devices and some attributes."""
     print("{:<15} {:<30} {:<}".format("Device Path", "Device Model",
                                       "Capacity (MB)"))
 
@@ -28,6 +14,9 @@ def show_devices():
 
 
 def select_device():
+    """Returns a device object, retrieved from the users selection.
+       Loops until a valid device is chosen, and the user has confirmed
+       their selection."""
     show_devices()
 
     while True:
@@ -48,18 +37,51 @@ def select_device():
             return device
 
 
+def wipe_device(device):
+    part_list = glob.glob('{}[0-9]*'.format(device.path)) 
+
+    for part in part_list:
+        with open(part, 'wb') as p:
+            p.write(bytearray(1024))
+
+    with open(device.path, 'wb') as d:
+        d.write(bytearray(1024))
+
+def prep_device(device):
+    """Recieves a device object.
+       Wipes the device and creates a single partition."""
+
+    #wipe_device(device)
+
+    disk = parted.freshDisk(device, 'msdos')
+    geometry = parted.Geometry(device=device, start=1, length=device.getLength() - 1)
+    filesystem = parted.FileSystem(type='ext4', geometry=geometry)
+    partition = parted.Partition(disk=disk, type=parted.PARTITION_NORMAL,
+                                 fs=filesystem, geometry=geometry)
+
+    disk.addPartition(partition=partition,
+                      constraint=device.optimalAlignedConstraint)
+
+    partition.setFlag(parted.PARTITION_BOOT)
+
+    disk.commit()
+
+
+def init_muck_disk(device):
+    partition_path = device.path + "1"
+    subprocess.run(["mkfs.ext4", partition_path])
+
+    mount_path = "/tmp/muck/mnt"
+
+    if not os.path.isdir(mount_path):
+        os.makedirs(mount_path)
+
+    subprocess.run(["mount", partition_path, mount_path])
+
+
+
 print("Welcome to MUCK!\n")
 
 working_device = select_device()
+
 prep_device(working_device)
-
-
-
-
-#try:
-#    device = parted.getDevice(device_path)
-#except parted.IOException:
-#    print(device_path + ": Device cannot be accessed or does not exist!")
-#    sys.exit(1)
-#else:
-#    print("Device valid. Continuing...")
